4b27193fe49a318cafc3a2e805a3529c991c29f9
Matheus Paixão <mpgs@cin.ufpe.br>
2021-12-19 12:21:56 -0300

    Refatoracao do metodo addUser

--- mtm-gui/src/app/usuario.service.ts
+++ mtm-gui/src/app/usuario.service.ts
@@ -4,10 +4,14 @@ export class UsuarioService {
   usuarios: Usuario[] = [];
   addUser(usuario: Usuario): Usuario | null {
     var result = null;
-    if (!this.usuarios.find(u => u.cpf == usuario.cpf) && !this.usuarios.find(us => us.email == usuario.email)) {
+    if (this.cpfEEmailNaoCadastrados(usuario.cpf, usuario.email)) {
       this.usuarios.push(usuario);
       result = usuario;
     }
     return result;
+  }
+
+  cpfEEmailNaoCadastrados(cpf: string, email: string): boolean {
+    return !this.usuarios.find(u => u.cpf == cpf) && !this.usuarios.find(us => us.email == email);
   }
 }
